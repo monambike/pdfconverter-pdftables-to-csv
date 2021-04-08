07744cb6180486ffb441e4ee7ae459e579ab1eed
monambike <vinicius_gabriel258@hotmail.com>
2021-04-08 15:02:53 -0300

    Fix index bug issue #27

--- bin/Debug/netcoreapp3.1/pdfconverter.py
+++ bin/Debug/netcoreapp3.1/pdfconverter.py
@@ -13,29 +13,20 @@ def Main():
 
 	# Pega todos os PDFs
 	chdir(folderPDFs)
+	
 	# Pega todos os PDFs dentro da pasta
+	indexFile = 0
 	for pdfFile in glob("*.pdf"):
 		try:
-			indexFile = 0
-
 			# Fazendo leitura do arquivo completo e passando para a variável
 			listOfDataFrames = tabula.read_pdf(pdfFile, pages="1", lattice=True, multiple_tables=True)
 
+			strIndexFile = str(indexFile)
+
 			# Indica que um arquivo completo foi lido com sucesso
 			print("-------------------------------------------------------------------")
-			print("CONVERSÃO (" + str(indexFile) + ")")
+			print("CONVERSÃO (" + strIndexFile + ")")
 			print("O arquivo " + pdfFile + " foi lido e está pronto pra ser convertido\n")
-			indexFile = indexFile = 1
-
-			# CÓDIGO PRECISA SER TESTADO E IMPLEMENTADO AINDA
-			# Faz a limpa de quebra de linha
-			#for row_index, row in df.iterrows():
-			#	if re.search(r"\\r", str(row)):
-			#		print type(row)               #Return type is pandas.Series
-			#		row.replace({r'\\r': ''} , regex=True)
-			#		print row
-			#		count += 1
-
 
 
 			# Para cada uma das tabelas 'DataFrames' contidos no arquivo csv completo 'lista de tabelas' será convertido
@@ -45,11 +36,12 @@ def Main():
 
 				# Indica que uma tabela foi convertida com sucesso
 				print("- O arquivo '" + pdfFile[:-4] + "-" + str(indexDataFrame) + ".csv' foi convertido")
-				indexDataFrame = indexDataFrame + 1
-
 				print(pandas.DataFrame(df))
 
+				indexDataFrame = indexDataFrame + 1
+
 			print("-------------------------------------------------------------------")
+			indexFile = indexFile + 1
 				
 		except Exception as err:
 			errorFile = open("../pdfconverter/bin/Debug/netcoreapp3.1/error-file.txt", "w")
@@ -59,10 +51,10 @@ def Main():
 			print("Ocorreu um erro, ao tentar converter"
 			"o arquivo")
 			breakline = input("-- pressione enter para continuar --")
-	#else:
-	#	errorFile = open("../pdfconverter/bin/Debug/netcoreapp3.1/error-file.txt", "w")
-	#	errorFile.write("Não há arquivos de PDF para serem convertidos.")
-	#	errorFile.close()
+	else:
+		errorFile = open("../pdfconverter/bin/Debug/netcoreapp3.1/error-file.txt", "w")
+		errorFile.write("Não há arquivos de PDF para serem convertidos.")
+		errorFile.close()
 
 def criarPastas():
 	# Cria as pastas necessárias para importar e exportar os arquivos 'não sobrescreve caso já exista'
