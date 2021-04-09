5d28797cb27dc45ea245d2057bbe5f4beeab6eba
monambike <vinicius_gabriel258@hotmail.com>
2021-04-09 11:24:51 -0300

    Add new extension (.xlsx) and enhance older ones

--- bin/Debug/netcoreapp3.1/pdfconverter.py
+++ bin/Debug/netcoreapp3.1/pdfconverter.py
@@ -1,5 +1,6 @@
 import pandas
 import tabula
+import xlsxwriter
 from os import chdir
 from glob import glob
 from pathlib import Path
@@ -7,13 +8,13 @@ from pathlib import Path
 folderPDFs = "../../../../PDFs"
 folderResultados = "../../../../resultados"
 
-# CONFIGURAÇÕES
+# CONFIGURAÇÕES DO PANDAS
 # Evita com que os dados acabem sendo quebrados
 pandas.options.display.expand_frame_repr = False
 # Fazer com que caso tenha um ';' ele não passe os dados pra outra célula
 pandas.options.display.latex.multicolumn = False
 #Define o padrão de codificação para UTF-8 com BOM
-pandas.options.display.encoding = "utf-8-sig"
+pandas.options.display.encoding = "utf-16"
 
 def Main():
 	criarPastas()
@@ -41,26 +42,36 @@ def Main():
 				# Para cada uma das tabelas 'DataFrames' contidos no arquivo csv completo 'lista de tabelas' será convertido
 				indexDataFrame = 0
 				for df in listOfDataFrames:
-					df = df.replace({r'\r': ''}, regex=True)
+					try:
+						df = df.replace({r'\r': ''}, regex=True)
 
-					fileName = pdfFile[:-4] + "-" + str(indexDataFrame)
+						fileName = pdfFile[:-4] + "-" + str(indexDataFrame)
 
-					df.to_csv("../resultados/tabelas/" + fileName + ".xls", line_terminator="\n", sep=";", index=False)
-					df.to_csv("../resultados/csv/" + fileName + ".csv", line_terminator="\n", sep=";", index=False)
-					df.to_csv("../resultados/texto/" + fileName + ".txt", line_terminator="\n", sep=";", index=False)
+						df.to_csv("../resultados/tabelas-csv/" + fileName + ".csv", line_terminator="\n", sep=";", index=False)
+						df.to_csv("../resultados/ponto_virgula-xls/" + fileName + ".xls", line_terminator="\n", sep=";", index=False)
+						df.to_csv("../resultados/ponto_virgula-txt/" + fileName + ".txt", line_terminator="\n", sep=";", index=False)
 
-					# Indica que uma tabela foi convertida com sucesso
-					print("- O arquivo '" + fileName + "' foi convertido")
-					print(pandas.DataFrame(df))
+						writer = pandas.ExcelWriter("../resultados/tabelas-xlsx/" + fileName + ".xlsx", engine='xlsxwriter')
+						df.to_excel(writer, engine="xlsxwriter", index=False)
+						writer.save()
 
-					indexDataFrame = indexDataFrame + 1
-
+						# Indica que uma tabela foi convertida com sucesso
+						print("- O arquivo '" + fileName + "' foi convertido")
+						print(pandas.DataFrame(df))
+						indexDataFrame = indexDataFrame + 1
+					except Exception as err:
+						print("Ocorreu um erro, ao tentar converter o arquivo")
+						print(err)
+						breakline = input("-- pressione enter para continuar --")
+						break
 				print("-------------------------------------------------------------------")
 				indexFile = indexFile + 1
 					
 			except Exception as err:
-				print("Ocorreu um erro, ao tentar converter o arquivo")
+				print("Ocorreu um erro, ao tentar ler o arquivo")
+				print(err)
 				breakline = input("-- pressione enter para continuar --")
+
 	except Exception as err:
 		print("Não há arquivos de PDF para serem convertidos.")
 		print("Erro:")
@@ -71,11 +82,9 @@ def criarPastas():
 	# Faz a verificação da existência das pastas a seguir e as cria caso elas ainda não existam
 	Path(folderPDFs).mkdir(parents=True, exist_ok=True)
 	Path(folderResultados).mkdir(parents=True, exist_ok=True)
-	Path(folderResultados + "/texto").mkdir(parents=True, exist_ok=True)
-	Path(folderResultados + "/tabelas").mkdir(parents=True, exist_ok=True)
-	Path(folderResultados + "/csv").mkdir(parents=True, exist_ok=True)
-
-
-#def definirConfiguracoesPanda():
+	Path(folderResultados + "/tabelas-csv").mkdir(parents=True, exist_ok=True)
+	Path(folderResultados + "/tabelas-xlsx").mkdir(parents=True, exist_ok=True)
+	Path(folderResultados + "/ponto_virgula-txt").mkdir(parents=True, exist_ok=True)
+	Path(folderResultados + "/ponto_virgula-xls").mkdir(parents=True, exist_ok=True)
 
 Main()
