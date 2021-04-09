619da637e873cb98a9c143c431181d24a139eb63
monambike <vinicius_gabriel258@hotmail.com>
2021-04-09 15:25:56 -0300

    Apply solutions to solve issue #30 #48

--- bin/Debug/netcoreapp3.1/pdfconverter.py
+++ bin/Debug/netcoreapp3.1/pdfconverter.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 folderPDFs = "../../../../PDFs"
 folderResultados = "../../../../resultados"
+outputFile = outputFile=open(folderResultados + "/output.txt", "a")
 
 # CONFIGURAÇÕES DO PANDAS
 # Evita com que os dados acabem sendo quebrados
@@ -36,9 +37,13 @@ def Main():
 				# Indica que um arquivo completo foi lido com sucesso
 				print("======================================================================\n"
 					"LEITURA DE ARQUIVO - NÚMERO " + str(indexFile) + "\n"
-					"O arquivo " + pdfFile + " foi lido e está pronto pra ser convertido\n"
-				)
+					"O arquivo " + pdfFile + " foi lido e está pronto pra ser convertido\n",
+				file=outputFile)
 
+				# Remove extensão do arquivo, pegando apenas o nome e atribui pra variavel
+				fileName = pdfFile[:-4]
+				# Cria uma pasta para armazenar tabelas com o mesmo nome pra cada arquivo
+				Path("../resultados/tabelas/" + fileName).mkdir(parents=True, exist_ok=True)
 
 				# Para cada uma das tabelas 'DataFrames' contidos no arquivo csv completo 'lista de tabelas' será convertido
 				indexDataFrame = 0
@@ -47,12 +52,9 @@ def Main():
 						# Removendo quebras de linha
 						df = df.replace({r'\r': ''}, regex=True)
 
-						fileName = pdfFile[:-4] + "-" + str(indexDataFrame)
+						df.to_csv("../resultados/texto/"+ fileName + ".txt", index=False, line_terminator="\n", sep=";", mode="a")
 
-						df.to_csv("../resultados/texto/" + fileName + ".txt", index=False, line_terminator="\n", sep=";")
-						df.to_csv("../resultados/teste.txt", index=False, line_terminator="\n", sep=";", mode="a")
-
-						writer = pandas.ExcelWriter("../resultados/tabelas/" + fileName + ".xlsx", engine='xlsxwriter')
+						writer = pandas.ExcelWriter("../resultados/tabelas/" + fileName + "/" + str(indexDataFrame) + ".xlsx", engine='xlsxwriter')
 						df.to_excel(writer, index=False, engine="xlsxwriter")
 						writer.save()
 
@@ -60,13 +62,13 @@ def Main():
 						print(
 							"______________________________________________________________________\n"
 							"A tabela da página "+ str(indexDataFrame + 1) + " do PDF foi convertida |\n"
-							"___________________________________________/\n"
-						)
-						print(pandas.DataFrame(df))
+							"___________________________________________/\n",
+						file=outputFile)
+						print(pandas.DataFrame(df), file=outputFile)
 						indexDataFrame = indexDataFrame + 1
 					except Exception as err: 
 						# Fecha o design
-						print("======================================================================")
+						print("======================================================================", file=outputFile)
 
 						print(
 							"**********************************************************************\n"
@@ -75,17 +77,15 @@ def Main():
 							"Ocorreu um erro, ao tentar converter o arquivo\n"
 							"\n"
 							"Exception: " + str(err) + "\n"
-							"**********************************************************************"
-						)
-						breakline = input("-- pressione enter para continuar --")
+							"**********************************************************************",
+						file=outputFile)
 						break
-				print("======================================================================")
+				print("======================================================================", file=outputFile)
 				indexFile = indexFile + 1
-				breakline = input("-- pressione enter para continuar --")
 					
 			except Exception as err:
 				# Fecha o design
-				print("======================================================================")
+				print("======================================================================", file=outputFile)
 
 				print(
 					"**********************************************************************\n"
@@ -93,21 +93,19 @@ def Main():
 					"Ocorreu um erro, ao tentar ler o arquivo\n"
 					"\n"
 					"Exception: " + str(err) + "\n"
-					"**********************************************************************"
-				)
-				breakline = input("-- pressione enter para continuar --")
+					"**********************************************************************",
+				file=outputFile)
 
 	except Exception as err:
 		# Fecha o design
-		print("======================================================================")
+		print("======================================================================", file=outputFile)
 
 		print(
 			"**********************************************************************\n"
 			"--- ERRO ---\n"
 			"Não há arquivos de PDF para serem convertidos\n"
-			"**********************************************************************"
-		)
-		breakline = input("-- pressione enter para continuar --")
+			"**********************************************************************",
+		file=outputFile)
 
 def criarPastas():
 	# Faz a verificação da existência das pastas a seguir e as cria caso elas ainda não existam
